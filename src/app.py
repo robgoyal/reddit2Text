4f6d947cbff7bb8a1cbfa22bd939e51c4a30f89a
Robin Goyal <goyal.rob@gmail.com>
2017-07-24 16:31:07 -0400

    Completed core logic

--- src/app.py
+++ src/app.py
@@ -27,55 +27,69 @@ def main():
     posts = get_posts(rows)
 
     # Create text for posts
-    compose_message(posts)
+    print(compose_message(posts))
 
 
 def compose_message(dict_posts):
 
+    # Retrieve environment variables for TWILIO client
     account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
     auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+
+    # Initialize twilio client
     client = Client(account_sid, auth_token)
 
+    # Text message format specified
     text = ""
     post_format = '-{:.50}: {}\n'
 
+    # Prepare text message with subreddit name with top post titles and links
     for post in dict_posts.keys():
         text += '{}\n'.format(post.upper())
-
         subreddit_posts = dict_posts[post]
+
         for sub_post in subreddit_posts:
             text += post_format.format(sub_post.title, shorten_url(sub_post.url))   
 
         text += '\n'
+    
+    # Send text with prepared message
+    # To and From must be authorized with Twilio account
+    message = client.messages.create(to="+12894892495",
+                                     from_="+12892051305",
+                                     body=text)
 
-   
-    print(text)
-    # print(message.sid)
+    return message.sid
 
 def get_posts(subreddits):
 
+    # Retrieve environment variables for REDDIT client
     client_id = os.environ.get('REDDIT_CLIENT_ID')
     secret_id = os.environ.get('REDDIT_SECRET_ID')
     user_agent = os.environ.get('REDDIT_USER_AGENT')
 
+    # Initialize reddit instance
     reddit = praw.Reddit(client_id = client_id, client_secret = secret_id,
                          user_agent = user_agent)
 
+    # Prepare dictionary of subreddit and submissions
     posts = {}
     for subreddit in subreddits:
 
+        # Retrieve top submissions for subreddit if it exists
         try:
-            submissions = list(reddit.subreddit(subreddit[0]).hot(limit = subreddit[1]))
+            submissions = list(reddit.subreddit(subreddit[0]).top(limit = subreddit[1]))
         except:
-            print('ERROR: The subreddit, {} doesn\'t exist'.format(subreddit[0]))
+            print('ERROR: The subreddit, {} doesn\'t exist. Delete using helper function.\n'.format(subreddit[0]))
 
+        # Set value of subreddit key to list of submissions
         posts[subreddit[0]] = submissions
 
     return posts
 
 def shorten_url(url):
 
-    # Grab API KEY from environment
+    # Retrieve API KEY for URL shortener API
     API_KEY = os.environ.get('GOOGLE_API_KEY')
 
     # Setup request for payload
@@ -83,7 +97,7 @@ def shorten_url(url):
     payload = json.dumps({'longUrl': url})
     headers = {'Content-Type': 'application/json'}
 
-    # 
+    # Request and return short url
     r = requests.post(post_url, data=payload, headers=headers)
     return r.json()['id']
 
