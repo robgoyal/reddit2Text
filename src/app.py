69cdc2f7d3e34f5852823b80d6ee19f477f13fd8
Robin Goyal <goyal.rob@gmail.com>
2017-07-24 15:09:18 -0400

    Implemented function to get top posts from subreddit

--- src/app.py
+++ src/app.py
@@ -3,6 +3,22 @@ from twilio.rest import Client
 import requests
 import json
 import os
+import sqlite3
+
+def get_posts(subreddits):
+    client_id = os.environ.get('REDDIT_CLIENT_ID')
+    secret_id = os.environ.get('REDDIT_SECRET_ID')
+    user_agent = os.environ.get('REDDIT_USER_AGENT')
+
+    reddit = praw.Reddit(client_id = client_id, client_secret = secret_id,
+                         user_agent = user_agent)
+
+    for subreddit in subreddits:
+        submissions = list(reddit.subreddit(subreddit[0]).hot(limit = subreddit[1]))
+        print("SUBREDDIT: %s\n" % subreddit[0])
+        for i in submissions:
+            print('{:.30}: {}\n'.format(i.title, i.url))
+
 
 def shorten_url(url):
     API_KEY = os.environ.get('GOOGLE_API_KEY')
@@ -12,4 +28,6 @@ def shorten_url(url):
     r = requests.post(post_url, data=payload, headers=headers)
     return r.json()['id']
 
-print(shorten_url('https://stackoverflow.com/questions/17357351/how-to-use-google-shortener-api-with-python'))
+get_posts([['learnpython', 5], ['learnprogramming', 2]])
+
+#print(shorten_url('https://stackoverflow.com/questions/17357351/how-to-use-google-shortener-api-with-python'))
